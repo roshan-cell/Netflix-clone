0520e5590bd8ae303de75050b44a504649c01fb7
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-04-19 22:00:23 +0530

    Login page

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,21 +1,52 @@
-const router = require("express").Router() ;
-const User = require("../models/User")
+const router = require("express").Router();
+const User = require("../models/User");
+const CryptoJS = require("crypto-js");
+const jwt = require("jsonwebtoken");
 
-// Register 
-router.post("/register" , async(req ,res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email ,
-        password: req.body.password
-    });
+// Register
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      JSON.stringify(req.body.password),
+      process.env.SECRET_KEY
+    ).toString(),
+  });
 
-    try{
-        const user = await newUser.save() ;
-        res.status(201).json(user) ;
+  try {
+    const user = await newUser.save();
+    res.status(201).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
-    } catch (err) {
-        res.status(500).json(err) ;
-    }
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong email or password");
+
+    // Decrypt
+    var bytes = CryptoJS.AES.decrypt(user.password , process.env.SECRET_KEY);
+    var originalPassword = JSON.parse(bytes.toString(CryptoJS.enc.Utf8));
+
+    originalPassword !== req.body.password &&
+        res.status(401).json("Wrong email or password !") ; 
+
+    const accessToken = jwt.sign(
+        {id: user._id , isAdmin: user.isAdmin} ,
+        process.env.SECRET_KEY,
+        {expiresIn: "5d"}
+    );
+        
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({...info , accessToken}) ; 
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
