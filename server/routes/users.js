281f17afec671111ec05672a8a5021e7cc5aac5d
roshan-cell <95081242+roshan-cell@users.noreply.github.com>
2023-04-29 22:12:40 +0530

    MOVIE_LIST

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,78 +1,99 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-const verify = require("../middleWare")
-
+const verify = require("../middleWare");
 
 // Update : for making any changes
-router.put("/:id" , verify , async(req,res) => {
-    if(req.user.id === req.params.id || req.user.isAdmin) {
-        if(req.body.password ) {
-            req.body.password = CryptoJS.AES.encrypt(
-                JSON.stringify(req.body.password),
-                process.env.SECRET_KEY
-              ).toString(); 
-        }
-
-        try{
-            const updatedUser = await User.findByIdAndUpdate(req.params.id , {
-                $set: req.body,
-            },
-            {new: true}
-            );
-            res.status(200).json(updatedUser);
-        } catch (err) {
-            res.status(500).json(err) ;
-        }
+router.put("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    if (req.body.password) {
+      req.body.password = CryptoJS.AES.encrypt(
+        JSON.stringify(req.body.password),
+        process.env.SECRET_KEY
+      ).toString();
     }
-    else{
-        res.status(403).json("You can only update your account!")
-    }
-})
 
+    try {
+      const updatedUser = await User.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+      res.status(200).json(updatedUser);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can only update your account!");
+  }
+});
 
 // DELETE
-router.delete("/:id" , verify , async(req,res) => {
-    if(req.user.id === req.params.id || req.user.isAdmin) {
-        try{
-            await User.findByIdAndDelete(req.params.id);
-            res.status(200).json("User has been deleted...")
-        } catch (err) {
-            res.status(500).json(err) ;
-        }
-    } else{
-        res.status(403).json("You can delete only your account!")
+router.delete("/:id", verify, async (req, res) => {
+  if (req.user.id === req.params.id || req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User has been deleted...");
+    } catch (err) {
+      res.status(500).json(err);
     }
-})
+  } else {
+    res.status(403).json("You can delete only your account!");
+  }
+});
 
 // Get a user
-router.get("/find/:id" , async(req,res) => {
-        try{
-            const user = await User.findById(req.params.id) ;
-            const { password , ...info } = user.__doc;
-            res.status(200).json(info) ; 
-        } catch (err) {
-            res.status(500).json(err) ;
-        }
+router.get("/find/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, ...info } = user.__doc;
+    res.status(200).json(info);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
-
-
 
 // GET ALL Users
-router.get("/" , verify , async(req,res) => {
-    const query = req.query.new;
-    if( req.user.isAdmin) {
-        try{
-            const users = query ? await User.find().limit(2) : await User.find() ;
-            res.status(200).json(users) ;
-        } catch (err) {
-            res.status(500).json(err) ;
-        }
-    } else{
-        res.status(403).json("You are not allowed to see all the users!")
+router.get("/", verify, async (req, res) => {
+  const query = req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users = query
+        ? await User.find().sort({ _id: -1 }).limit(2)
+        : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
     }
+  } else {
+    res.status(403).json("You are not allowed to see all the users!");
+  }
 });
 
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  const latYear = today.setFullYear(today.setFullYear() - 1);
 
-// GET ALL
-module.exports = router ;
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      },
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
